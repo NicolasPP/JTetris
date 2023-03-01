fd83a8db8c80a6058619efb4724bc11c8fa54bd2
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-01 20:50:01 +0700

    simple pair class to hold two values

---
+++ core/src/com/nicolas/tetris/utils/Pair.java
@@ -0,0 +1,22 @@
+package com.nicolas.tetris.utils;
+
+public class Pair<K, V> {
+    private final K first;
+    private final V second;
+
+    public static <K, V> Pair<K, V> createPair(K first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    public Pair(K f, V s){
+        first = f;
+        second = s;
+    }
+
+    public K getFirst() {
+        return first;
+    }
+    public V getSecond() {
+        return second;
+    }
+}
