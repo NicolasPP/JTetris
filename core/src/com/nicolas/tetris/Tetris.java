7f4d9f4cc414da636ca185d7db4f2307a8a4fda6
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-01 20:53:38 +0700

    changed render function to accommodate for all changes made

--- core/src/com/nicolas/tetris/Tetris.java
+++ core/src/com/nicolas/tetris/Tetris.java
@@ -1,33 +1,33 @@
 package com.nicolas.tetris;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.nicolas.tetris.manager.ResourceManager;
-import jdk.internal.loader.Resource;
+import com.nicolas.tetris.manager.SpriteManager;
 
 public class Tetris extends ApplicationAdapter {
-	SpriteBatch batch;
+    SpriteBatch batch;
+    SpriteManager spriteManager;
 
-	@Override
-	public void create () {
-		ResourceManager.loadData();
-		batch = new SpriteBatch();
-	}
+    @Override
+    public void create() {
+        ResourceManager.loadData();
+        batch = new SpriteBatch();
+        spriteManager = new SpriteManager();
+    }
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		ResourceManager.get("Board").draw(batch);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+    @Override
+    public void render() {
+        spriteManager.update();
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        spriteManager.render(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
