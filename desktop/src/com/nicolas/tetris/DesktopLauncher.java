ad7881d7fd40d8690e7bda6d9bba465f769f6325
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-02 11:38:30 +0700

    for DesktopLauncher setResizable and setWindowedMode

--- desktop/src/com/nicolas/tetris/DesktopLauncher.java
+++ desktop/src/com/nicolas/tetris/DesktopLauncher.java
@@ -2,14 +2,17 @@ package com.nicolas.tetris;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nicolas.tetris.Tetris;
 
+import static com.nicolas.tetris.config.TetrisConfig.WINDOW_WIDTH;
+import static com.nicolas.tetris.config.TetrisConfig.WINDOW_HEIGHT;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("JTetris");
+		config.setResizable(false);
+		config.setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGHT);
 		new Lwjgl3Application(new Tetris(), config);
 	}
 }
