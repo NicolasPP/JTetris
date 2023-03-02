84c51b7bb6f65ab74394a8200dceb93686950bfe
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-02 18:48:40 +0700

    set the input processor in SpriteManager init function

--- core/src/com/nicolas/tetris/manager/SpriteManager.java
+++ core/src/com/nicolas/tetris/manager/SpriteManager.java
@@ -1,5 +1,6 @@
 package com.nicolas.tetris.manager;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -33,42 +34,36 @@ import static com.nicolas.tetris.config.TetrisConfig.GREEN;
 import static com.nicolas.tetris.config.TetrisConfig.PURPLE;
 import static com.nicolas.tetris.config.TetrisConfig.RED;
 
-public class SpriteManager implements InputProcessor
-{
-    final ArrayList<TetrominoSprite> tetrominos = new ArrayList<>();
+public class SpriteManager implements InputProcessor {
+    final ArrayList<TetrominoSprite> tetrominos;
     final BoardSprite board;
 
     public SpriteManager() {
-
         board = new BoardSprite(new Vector2(0, 0));
+        tetrominos = new ArrayList<>();
+        init();
+    }
 
+    private void init(){
         tetrominos.add(new TetrominoSprite(SHAPE_I_NAME, GHOST_SHAPE_I_NAME, LIGHT_BLUE));
-
         tetrominos.add(new TetrominoSprite(SHAPE_J_NAME, GHOST_SHAPE_J_NAME, BLUE));
-
         tetrominos.add(new TetrominoSprite(SHAPE_L_NAME, GHOST_SHAPE_L_NAME, ORANGE));
-
         tetrominos.add(new TetrominoSprite(SHAPE_O_NAME, GHOST_SHAPE_O_NAME, YELLOW));
-
         tetrominos.add(new TetrominoSprite(SHAPE_S_NAME, GHOST_SHAPE_S_NAME, GREEN));
-
         tetrominos.add(new TetrominoSprite(SHAPE_T_NAME, GHOST_SHAPE_T_NAME, PURPLE));
-
         tetrominos.add(new TetrominoSprite(SHAPE_Z_NAME, GHOST_SHAPE_Z_NAME, RED));
+
+        Gdx.input.setInputProcessor(this);
     }
 
     public void render(SpriteBatch batch) {
         board.render(batch);
-        for (TetrominoSprite t : tetrominos) {
-            t.render(batch);
-        }
+        tetrominos.forEach(tetrominoSprite -> tetrominoSprite.render(batch));
     }
 
     public void update() {
         board.update();
-        for (TetrominoSprite t : tetrominos) {
-            t.update();
-        }
+        tetrominos.forEach(TetrominoSprite::update);
     }
 
     @Override
