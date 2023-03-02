274c1c6743ea5a8c781dc79f7a1310b8686c7ab5
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-02 09:46:12 +0700

    using new config class
    chnaged validateScale to use CELL_SIZE rather than calculating the sprites width and height

--- core/src/com/nicolas/tetris/sprites/TetrisSprite.java
+++ core/src/com/nicolas/tetris/sprites/TetrisSprite.java
@@ -2,27 +2,29 @@ package com.nicolas.tetris.sprites;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.nicolas.tetris.manager.ResourceManager;
-import com.nicolas.tetris.utils.Pair;
 
 import java.util.HashMap;
 import java.util.List;
+
+import static com.nicolas.tetris.config.TetrisConfig.CELL_SIZE;
 
 
 public abstract class TetrisSprite {
     Sprite sprite;
     HashMap<String, Sprite> subSprites;
-    Pair<Integer, Integer> bottomLeft;
+    Vector2 bottomLeft;
     final private float scale;
 
     public TetrisSprite(
             String spriteName,
             List<String> subSpriteNames,
-            Pair<Integer, Integer> bottomLeft,
+            Vector2 bottomLeft,
             float scale
     ) {
         this.sprite = ResourceManager.get(spriteName);
-        this.scale = validateScale(scale);
+        this.scale = TetrisSprite.validateScale(scale);
         this.bottomLeft = bottomLeft;
         this.subSprites = new HashMap<>();
         init(subSpriteNames);
@@ -41,18 +43,15 @@ public abstract class TetrisSprite {
         for (String subSpriteName : subSpriteNames) {
             subSprites.put(subSpriteName, ResourceManager.get(subSpriteName));
             scaleSprite(subSprites.get(subSpriteName));
-            subSprites.get(subSpriteName).setPosition(bottomLeft.getFirst(), bottomLeft.getSecond());
+            subSprites.get(subSpriteName).setPosition(bottomLeft.x, bottomLeft.y);
 
         }
-        sprite.setPosition(bottomLeft.getFirst(), bottomLeft.getSecond());
+        sprite.setPosition(bottomLeft.x, bottomLeft.y);
     }
 
-    private float validateScale(float scale) {
-        if (sprite.getHeight() % (scale * sprite.getHeight()) != 0 ||
-                sprite.getWidth() % (scale * sprite.getWidth()) != 0) {
+    static private float validateScale(float scale) {
 //            invalid scale value
-            return 1f;
-        }
+        if (CELL_SIZE % (scale * CELL_SIZE) != 0) { return 1f; }
         return scale;
     }
 }
