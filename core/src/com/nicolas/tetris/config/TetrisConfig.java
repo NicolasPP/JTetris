491cc86d5216060f1b4ca48c03b89a6eaded3034
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-02 09:45:22 +0700

    added TetrisConfig class to store all constant values

---
+++ core/src/com/nicolas/tetris/config/TetrisConfig.java
@@ -0,0 +1,31 @@
+package com.nicolas.tetris.config;
+
+public class TetrisConfig {
+    final static public String BG_GRAY_NAME = "BG_gray";
+    final static public String BG_BLACK_NAME = "BG_black";
+    final static public String BORDER_NAME = "Border";
+    final static public String BOARD_NAME = "Board";
+    final static public String SHAPE_I_NAME = "shape_I";
+    final static public String SHAPE_J_NAME = "shape_J";
+    final static public String SHAPE_L_NAME = "shape_L";
+    final static public String SHAPE_O_NAME = "shape_O";
+    final static public String SHAPE_S_NAME = "shape_S";
+    final static public String SHAPE_T_NAME = "shape_T";
+    final static public String SHAPE_Z_NAME = "shape_Z";
+    final static public String LIGHT_BLUE = "LightBlue";
+    final static public String BLUE = "Blue";
+    final static public String ORANGE = "Orange";
+    final static public String YELLOW = "Yellow";
+    final static public String GREEN = "Green";
+    final static public String PURPLE = "Purple";
+    final static public String RED = "Red";
+    final static public String GHOST_SHAPE_I_NAME = "ghost_shape_I";
+    final static public String GHOST_SHAPE_J_NAME = "ghost_shape_J";
+    final static public String GHOST_SHAPE_L_NAME = "ghost_shape_L";
+    final static public String GHOST_SHAPE_O_NAME = "ghost_shape_O";
+    final static public String GHOST_SHAPE_S_NAME = "ghost_shape_S";
+    final static public String GHOST_SHAPE_T_NAME = "ghost_shape_T";
+    final static public String GHOST_SHAPE_Z_NAME = "ghost_shape_Z";
+    final static public int CELL_SIZE = 64;
+
+}
