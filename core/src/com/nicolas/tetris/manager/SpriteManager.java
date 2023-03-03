cd4d3fc526680160203492492cf0391a10a8c297
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-03 09:56:43 +0700

    added cellType back to TetrominoSprite

--- core/src/com/nicolas/tetris/manager/SpriteManager.java
+++ core/src/com/nicolas/tetris/manager/SpriteManager.java
@@ -9,43 +9,13 @@ import com.nicolas.tetris.game.GameState;
 import com.nicolas.tetris.sprites.BoardSprite;
 import com.nicolas.tetris.sprites.TetrominoSprite;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_I_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_J_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_L_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_O_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_S_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_T_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_Z_NAME;
-
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_I_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_J_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_L_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_O_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_S_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_T_NAME;
-import static com.nicolas.tetris.config.TetrisConfig.GHOST_SHAPE_Z_NAME;
-
-
-import static com.nicolas.tetris.config.TetrisConfig.LIGHT_BLUE;
-import static com.nicolas.tetris.config.TetrisConfig.BLUE;
-import static com.nicolas.tetris.config.TetrisConfig.ORANGE;
-import static com.nicolas.tetris.config.TetrisConfig.YELLOW;
-import static com.nicolas.tetris.config.TetrisConfig.GREEN;
-import static com.nicolas.tetris.config.TetrisConfig.PURPLE;
-import static com.nicolas.tetris.config.TetrisConfig.RED;
-
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_I_MAP;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_J_MAP;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_L_MAP;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_O_MAP;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_S_MAP;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_T_MAP;
-import static com.nicolas.tetris.config.TetrisConfig.SHAPE_Z_MAP;
+import static com.nicolas.tetris.config.TetrisConfig.*;
 
 public class SpriteManager implements InputProcessor {
-    private final ArrayList<TetrominoSprite> tetrominos;
+    private final HashMap<CellType, TetrominoSprite> tetrominos;
     private final BoardSprite board;
 
     private final GameState gameState;
@@ -53,32 +23,37 @@ public class SpriteManager implements InputProcessor {
 
 
     public SpriteManager() {
-        board = new BoardSprite(new Vector2(0, 0));
-        tetrominos = new ArrayList<>();
-        gameState = new GameState();
+        board = new BoardSprite();
+        tetrominos = new HashMap<>();
+        gameState = new GameState(new Vector2(0, 0));
         init();
     }
 
     private void init() {
         Gdx.input.setInputProcessor(this);
 
-        tetrominos.add(new TetrominoSprite(SHAPE_I_NAME, GHOST_SHAPE_I_NAME, LIGHT_BLUE, CellType.I, SHAPE_I_MAP));
-        tetrominos.add(new TetrominoSprite(SHAPE_J_NAME, GHOST_SHAPE_J_NAME, BLUE, CellType.J, SHAPE_J_MAP));
-        tetrominos.add(new TetrominoSprite(SHAPE_L_NAME, GHOST_SHAPE_L_NAME, ORANGE, CellType.L, SHAPE_L_MAP));
-        tetrominos.add(new TetrominoSprite(SHAPE_O_NAME, GHOST_SHAPE_O_NAME, YELLOW, CellType.O, SHAPE_O_MAP));
-        tetrominos.add(new TetrominoSprite(SHAPE_S_NAME, GHOST_SHAPE_S_NAME, GREEN, CellType.S, SHAPE_S_MAP));
-        tetrominos.add(new TetrominoSprite(SHAPE_T_NAME, GHOST_SHAPE_T_NAME, PURPLE, CellType.T, SHAPE_T_MAP));
-        tetrominos.add(new TetrominoSprite(SHAPE_Z_NAME, GHOST_SHAPE_Z_NAME, RED, CellType.Z, SHAPE_Z_MAP));
+        tetrominos.put(CellType.I, new TetrominoSprite(CellType.I, SHAPE_I_NAME, GHOST_SHAPE_I_NAME, LIGHT_BLUE, SHAPE_I_MAP));
+        tetrominos.put(CellType.J, new TetrominoSprite(CellType.J, SHAPE_J_NAME, GHOST_SHAPE_J_NAME, BLUE, SHAPE_J_MAP));
+        tetrominos.put(CellType.L, new TetrominoSprite(CellType.L, SHAPE_L_NAME, GHOST_SHAPE_L_NAME, ORANGE, SHAPE_L_MAP));
+        tetrominos.put(CellType.O, new TetrominoSprite(CellType.O, SHAPE_O_NAME, GHOST_SHAPE_O_NAME, YELLOW, SHAPE_O_MAP));
+        tetrominos.put(CellType.S, new TetrominoSprite(CellType.S, SHAPE_S_NAME, GHOST_SHAPE_S_NAME, GREEN, SHAPE_S_MAP));
+        tetrominos.put(CellType.T, new TetrominoSprite(CellType.T, SHAPE_T_NAME, GHOST_SHAPE_T_NAME, PURPLE, SHAPE_T_MAP));
+        tetrominos.put(CellType.Z, new TetrominoSprite(CellType.Z, SHAPE_Z_NAME, GHOST_SHAPE_Z_NAME, RED, SHAPE_Z_MAP));
     }
 
     public void render(SpriteBatch batch) {
-        board.render(batch);
-        tetrominos.forEach(tetrominoSprite -> tetrominoSprite.render(batch));
+        board.render(batch, gameState.getBoardPos());
+
+        Arrays.stream(gameState.getState()).forEach(row ->
+                Arrays.stream(row).forEach(cell ->
+                        tetrominos.get(CellType.O).render(batch, cell.getBottomLeft())
+                )
+        );
     }
 
     public void update() {
         board.update();
-        tetrominos.forEach(TetrominoSprite::update);
+        tetrominos.forEach((type, tetromino) -> tetromino.update());
     }
 
     @Override
