45091c65ce673bb09241d30a6da204db920d3b9a
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-03 09:56:09 +0700

    added cellType back to TetrominoSprite and added some getters

--- core/src/com/nicolas/tetris/sprites/TetrominoSprite.java
+++ core/src/com/nicolas/tetris/sprites/TetrominoSprite.java
@@ -2,16 +2,19 @@ package com.nicolas.tetris.sprites;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.nicolas.tetris.game.CellType;
 
 import java.util.Arrays;
 
 public class TetrominoSprite extends TetrisSprite {
-    String ghostName;
-    String colorName;
+    private final String ghostName;
+    private final String colorName;
+    private final CellType cellType;
 
     int[][] cellMap;
 
     public TetrominoSprite(
+            CellType type,
             String textureName,
             String ghostTextureName,
             String colorTextureName,
@@ -21,6 +24,7 @@ public class TetrominoSprite extends TetrisSprite {
         ghostName = ghostTextureName;
         colorName = colorTextureName;
         cellMap = cMap;
+        cellType = type;
     }
 
     @Override
@@ -32,4 +36,9 @@ public class TetrominoSprite extends TetrisSprite {
     public void update() {
 //        System.out.println("Not Implemented");
     }
+
+    public int[][] getCellMap() {return cellMap;}
+    public CellType getCellType() {return cellType;}
+    public String getGhostName() {return ghostName;}
+    public String getColorName() {return colorName;}
 }
