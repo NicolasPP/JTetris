d3617cfb3558a25d22c1140edaeefa2f12d099fa
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-03 09:58:03 +0700

    added enum UpdateType for each cell of the gameState

--- core/src/com/nicolas/tetris/game/CellType.java
+++ core/src/com/nicolas/tetris/game/CellType.java
@@ -1,5 +1,5 @@
 package com.nicolas.tetris.game;
 
 public enum CellType {
-    EMPTY, SPAWN, I, J, L, O, S, T, Z
+    EMPTY, I, J, L, O, S, T, Z
 }
---
+++ core/src/com/nicolas/tetris/game/UpdateType.java
@@ -0,0 +1,5 @@
+package com.nicolas.tetris.game;
+
+public enum UpdateType {
+    EMPTY, FALLEN, FALLING
+}
