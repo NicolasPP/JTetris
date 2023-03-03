cb84cab29ead3c1660285aeb93cde53913cc5027
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-03 09:58:33 +0700

    implemented Cell class which will be the core of the game state

---
+++ core/src/com/nicolas/tetris/game/Cell.java
@@ -0,0 +1,14 @@
+package com.nicolas.tetris.game;
+
+import com.badlogic.gdx.math.Vector2;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Cell {
+    private CellType type;
+
+    private UpdateType updateType;
+    private Vector2 bottomLeft;
+}
