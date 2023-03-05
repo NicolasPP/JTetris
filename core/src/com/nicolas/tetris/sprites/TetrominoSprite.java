9e0e8624ec81b23370db91a0bd75734b36a13078
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-05 19:01:05 +0700

    added Cell::isFalling to Cell class for filter function

--- core/src/com/nicolas/tetris/sprites/TetrominoSprite.java
+++ core/src/com/nicolas/tetris/sprites/TetrominoSprite.java
@@ -3,8 +3,13 @@ package com.nicolas.tetris.sprites;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.nicolas.tetris.game.CellType;
+import com.nicolas.tetris.game.TetrominoState;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static com.nicolas.tetris.config.TetrisConfig.SPAWN_COl;
+import static com.nicolas.tetris.config.TetrisConfig.SPAWN_ROW;
 
 public class TetrominoSprite extends TetrisSprite {
     private final String ghostName;
@@ -35,5 +40,19 @@ public class TetrominoSprite extends TetrisSprite {
     }
 
     public int[][] getCellMap() {return cellMap;}
+    public int[][] getCellMapClone() {
+        if (cellMap == null) return null;
+        final int[][] cellMapClone = new int[cellMap.length][];
+        IntStream.range(0, cellMap.length).forEach(index -> cellMapClone[index] = cellMap[index].clone());
+        return cellMapClone;
+    }
     public CellType getCellType() {return cellType;}
+
+    public TetrominoState getNewState(){
+        return TetrominoState.builder()
+                .cellMap(getCellMapClone())
+                .type(cellType)
+                .pos(new Vector2(SPAWN_ROW, SPAWN_COl))
+                .build();
+    }
 }
