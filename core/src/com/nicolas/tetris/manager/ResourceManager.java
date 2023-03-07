0248889682f7a59fbf3deafc655228b7f481371a
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-07 09:52:55 +0700

    added method for loading the fonts in ResourceManager

--- core/src/com/nicolas/tetris/manager/ResourceManager.java
+++ core/src/com/nicolas/tetris/manager/ResourceManager.java
@@ -1,17 +1,24 @@
 package com.nicolas.tetris.manager;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+import static com.nicolas.tetris.config.TetrisConfig.*;
 
 public class ResourceManager {
     private static TextureAtlas texturesAtlas;
-
+    private static BitmapFont font;
     public static void loadData() {
-        texturesAtlas = new TextureAtlas("sprites.txt");
+        texturesAtlas = new TextureAtlas(SPRITE_TXT_FILE);
+        font = generateFont(PIXEL_FONT_TTF);
     }
 
-    public static TextureRegion get(String name) {
+    public static BitmapFont getFont() {return font;}
+    public static TextureRegion getTexture(String name) {
         Sprite sprite = texturesAtlas.createSprite(name);
         return new TextureRegion(
                 sprite.getTexture(),
@@ -20,5 +27,14 @@ public class ResourceManager {
                 sprite.getRegionWidth(),
                 sprite.getRegionHeight()
         );
+    }
+
+    private static BitmapFont generateFont(String fontFile){
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(fontFile));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 12;
+        BitmapFont generatedFont = generator.generateFont(parameter);
+        generator.dispose();
+        return generatedFont;
     }
 }
