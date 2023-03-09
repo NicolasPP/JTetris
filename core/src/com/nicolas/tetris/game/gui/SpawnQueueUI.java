45d48ecf026b6cc510a4c10e99406f4903821064
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-09 16:15:20 +0700

    re-implemented spawnQueue using vectors optimized spawn q to only calculate position when we spawn a piece

--- core/src/com/nicolas/tetris/game/gui/SpawnQueueUI.java
+++ core/src/com/nicolas/tetris/game/gui/SpawnQueueUI.java
@@ -1,70 +1,79 @@
 package com.nicolas.tetris.game.gui;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.nicolas.tetris.game.cell.CellType;
 import com.nicolas.tetris.game.randomizer.SpriteBagRand;
 import com.nicolas.tetris.sprites.TetrominoSprite;
-import com.nicolas.tetris.utils.Pos;
 
-import static com.nicolas.tetris.config.TetrisConfig.SCORE_UI_HEIGHT;
+import static com.nicolas.tetris.config.TetrisConfig.SCORE_UI_ROWS;
 import static com.nicolas.tetris.config.TetrisConfig.CELL_SIZE;
 import static com.nicolas.tetris.config.TetrisConfig.TEXTURE_SCALE;
-import static com.nicolas.tetris.config.TetrisConfig.STATS_UI_WIDTH;
+import static com.nicolas.tetris.config.TetrisConfig.STATS_UI_COLS;
 import static com.nicolas.tetris.config.TetrisConfig.BOARD_COLS;
-import static com.nicolas.tetris.config.TetrisConfig.SPAWN_QUEUE_UI_WIDTH;
-import static com.nicolas.tetris.config.TetrisConfig.SPAWN_QUEUE_UI_HEIGHT;
+import static com.nicolas.tetris.config.TetrisConfig.SPAWN_QUEUE_UI_COLS;
+import static com.nicolas.tetris.config.TetrisConfig.SPAWN_QUEUE_UI_ROWS;
 import static com.nicolas.tetris.config.TetrisConfig.BG_BLACK_NAME;
 
 public class SpawnQueueUI extends UIComponent {
+
+    final private Vector2[] positions;
     SpriteBagRand bagRandomizer;
 
     public SpawnQueueUI(SpriteBagRand randomizer) {
-        super(new Pos(SCORE_UI_HEIGHT * (int) (CELL_SIZE * TEXTURE_SCALE),
-                        (STATS_UI_WIDTH + BOARD_COLS) * (int) (CELL_SIZE * TEXTURE_SCALE)),
-                SPAWN_QUEUE_UI_WIDTH, SPAWN_QUEUE_UI_HEIGHT);
+        super(new Vector2((STATS_UI_COLS + BOARD_COLS) * (int) (CELL_SIZE * TEXTURE_SCALE),
+                        SCORE_UI_ROWS * (int) (CELL_SIZE * TEXTURE_SCALE)),
+                SPAWN_QUEUE_UI_COLS, SPAWN_QUEUE_UI_ROWS);
         bagRandomizer = randomizer;
+        positions = new Vector2[randomizer.peekWholeQueue().length];
+        updatePositions();
     }
 
-    private int getTotalHeight() {
-        int totalHeight = 0;
-        for (CellType type : bagRandomizer.peekQueue()) {
-            int height = (int) TetrominoSprite.get(type).getTextureSize().y;
-            if (type == CellType.I) height = CELL_SIZE;
-            totalHeight += height;
+    public void updatePositions(){
+        Vector2 currentPos = new Vector2(getBottomLeft());
+
+        CellType[] queueWholePeek = bagRandomizer.peekWholeQueue();
+
+        int totalRows = 0;
+        for (CellType type : queueWholePeek) {
+            if (type == CellType.I) totalRows++;
+            else totalRows += 2;
         }
-        return totalHeight;
+
+        float vertSpacing = (getSize().y - (totalRows * (CELL_SIZE * TEXTURE_SCALE))) / 8;
+
+        for (int index = queueWholePeek.length - 1; index >= 0; index--) {
+            CellType type = queueWholePeek[index];
+            TetrominoSprite tetromino = TetrominoSprite.get(type);
+
+            float tetrominoW = tetromino.getTextureSize().x;
+            float tetrominoH = tetromino.getTextureSize().y;
+
+            float horzSpacing = (getSize().x - tetrominoW) / 2;
+
+            currentPos.x = getBottomLeft().x + horzSpacing;
+            currentPos.y += vertSpacing;
+
+            if (type == CellType.I) {
+                currentPos.x = getBottomLeft().x;
+                tetrominoH = tetromino.getTextureSize().x;
+            }
+
+            positions[index] = new Vector2(currentPos);
+
+            currentPos.y += tetrominoH;
+        }
     }
 
     @Override
     public void render(SpriteBatch batch) {
         renderBackGround(batch, BG_BLACK_NAME);
-        Pos currentPos = new Pos(getBottomLeft());
-        CellType[] queuePeek = bagRandomizer.peekQueue();
+        CellType[] queueWholePeek = bagRandomizer.peekWholeQueue();
 
-        int vertSpacing = (getHeightUI() - getTotalHeight()) / (queuePeek.length + 1);
-
-        boolean rotate;
-        for (int index = queuePeek.length - 1; index >= 0; index--) {
-            CellType type = queuePeek[index];
+        for (int index = queueWholePeek.length - 1; index >= 0; index--) {
+            CellType type = queueWholePeek[index];
             TetrominoSprite tetromino = TetrominoSprite.get(type);
-
-            int horzSpacing = (getWidthUI() - (int) tetromino.getTextureSize().x) / 2;
-
-            currentPos.setRow(currentPos.getRow() + vertSpacing);
-            currentPos.setCol(getBottomLeft().getCol() + horzSpacing);
-
-
-            rotate = false;
-            Pos pos = new Pos((int) tetromino.getTextureSize().y, 0);
-            if (type == CellType.I) {
-                currentPos.setCol(getBottomLeft().getCol());
-                pos.setRow((int) tetromino.getTextureSize().x);
-                rotate = true;
-            }
-
-            tetromino.renderTexture(batch, currentPos, rotate);
-
-            currentPos.increment(pos);
+            tetromino.renderTexture(batch, positions[index], type == CellType.I);
         }
     }
 }
