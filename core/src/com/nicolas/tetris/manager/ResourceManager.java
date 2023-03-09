02b6cd6b7396795b89db4843c033488aea9fff56
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-09 16:04:06 +0700

    added ResourceManager.Fonts with PIXEL and TETRIS the two available fonts
    added method to Fonts to get fileName
    
    getFont now returns you a new font. with specified styling

--- core/src/com/nicolas/tetris/manager/ResourceManager.java
+++ core/src/com/nicolas/tetris/manager/ResourceManager.java
@@ -7,17 +7,34 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
-import static com.nicolas.tetris.config.TetrisConfig.*;
+import static com.nicolas.tetris.config.TetrisConfig.TETRIS_FONT_TTF;
+import static com.nicolas.tetris.config.TetrisConfig.PIXEL_FONT_TTF;
+import static com.nicolas.tetris.config.TetrisConfig.SPRITE_TXT_FILE;
 
 public class ResourceManager {
     private static TextureAtlas texturesAtlas;
-    private static BitmapFont font;
-    public static void loadData(String fontFile) {
+    public enum Fonts{
+        PIXEL, TETRIS;
+
+        public String getFile(){
+            switch (this){
+                case PIXEL: return PIXEL_FONT_TTF;
+                case TETRIS: return TETRIS_FONT_TTF;
+
+            }
+            return PIXEL_FONT_TTF;
+        }
+    }
+    public static void loadData() {
         texturesAtlas = new TextureAtlas(SPRITE_TXT_FILE);
-        font = generateFont(fontFile);
     }
 
-    public static BitmapFont getFont() {return font;}
+    public static BitmapFont getFont(Fonts font, FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(font.getFile()));
+        BitmapFont generatedFont = generator.generateFont(parameter);
+        generator.dispose();
+        return generatedFont;
+    }
     public static TextureRegion getTexture(String name) {
         Sprite sprite = texturesAtlas.createSprite(name);
         return new TextureRegion(
@@ -27,14 +44,5 @@ public class ResourceManager {
                 sprite.getRegionWidth(),
                 sprite.getRegionHeight()
         );
-    }
-
-    private static BitmapFont generateFont(String fontFile){
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(fontFile));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 12;
-        BitmapFont generatedFont = generator.generateFont(parameter);
-        generator.dispose();
-        return generatedFont;
     }
 }
