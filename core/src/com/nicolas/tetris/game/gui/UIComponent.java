cf85ec3b0d22da46998d0f1324797aefb08135b6
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-09 16:13:14 +0700

    changed UI component to store 2 vector2s one for position the other for size

--- core/src/com/nicolas/tetris/game/gui/UIComponent.java
+++ core/src/com/nicolas/tetris/game/gui/UIComponent.java
@@ -1,8 +1,8 @@
 package com.nicolas.tetris.game.gui;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.nicolas.tetris.sprites.BoardSprite;
-import com.nicolas.tetris.utils.Pos;
 import lombok.Data;
 
 import java.util.stream.IntStream;
@@ -12,29 +12,28 @@ import static com.nicolas.tetris.config.TetrisConfig.TEXTURE_SCALE;
 
 @Data
 public abstract class UIComponent {
-    private Pos bottomLeft;
-    private int width;
-    private int height;
+    private Vector2 bottomLeft;
+    private Vector2 size;
+    private int cols;
+    private int rows;
 
-    public UIComponent(Pos pos, int w, int h){
+    public UIComponent(Vector2 pos, int c, int r) {
         bottomLeft = pos;
-        width = w;
-        height = h;
+        size = new Vector2(
+                c * (CELL_SIZE * TEXTURE_SCALE),
+                r * (CELL_SIZE * TEXTURE_SCALE));
+        cols = c;
+        rows = r;
     }
 
-    public int getHeightUI(){
-        return (int) (CELL_SIZE * TEXTURE_SCALE) * height;
-    }
-    public int getWidthUI(){
-        return (int) (CELL_SIZE * TEXTURE_SCALE) * width;
-    }
     public abstract void render(SpriteBatch batch);
 
-    public void renderBackGround(SpriteBatch batch, String subTextureName){
+    public void renderBackGround(SpriteBatch batch, String subTextureName) {
         int cellSize = (int) (CELL_SIZE * TEXTURE_SCALE);
-        IntStream.range(0, height).forEach(row -> IntStream.range(0, width).forEach(col ->
-            BoardSprite.getInstance().renderSubTexture(batch, subTextureName,
-            new Pos(bottomLeft.getRow() + (cellSize * row), bottomLeft.getCol() + (cellSize * col)))
+        IntStream.range(0, rows).forEach(row -> IntStream.range(0, cols).forEach(col ->
+                BoardSprite.getInstance().renderSubTexture(batch, subTextureName,
+                        new Vector2(bottomLeft.x + (cellSize * col), bottomLeft.y + (cellSize * row)),
+                        BoardSprite.getInstance().getSubTextureSize(), false)
         ));
     }
 }
