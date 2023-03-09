99d3f9f65c9f07463d5148fdfb6a6018e6e3b445
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-09 16:11:58 +0700

    reformatting

--- core/src/com/nicolas/tetris/game/randomizer/SpriteBagRand.java
+++ core/src/com/nicolas/tetris/game/randomizer/SpriteBagRand.java
@@ -3,32 +3,41 @@ package com.nicolas.tetris.game.randomizer;
 import com.nicolas.tetris.game.cell.CellType;
 import com.nicolas.tetris.sprites.TetrominoSprite;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SpriteBagRand {
     private final LinkedList<CellType> queue = new LinkedList<>();
     private final LinkedList<CellType> stock = new LinkedList<>();
     private final List<CellType> bag = Arrays.asList(
             CellType.I, CellType.L, CellType.J, CellType.S, CellType.O, CellType.T, CellType.Z);
-    public SpriteBagRand(){
+
+    public SpriteBagRand() {
         Collections.shuffle(bag);
         queue.addAll(bag);
     }
 
-    private void restock(){
+    private void restock() {
         Collections.shuffle(bag);
         stock.addAll(bag);
     }
 
-    public TetrominoSprite getNext(){
+    public TetrominoSprite getNext() {
         if (stock.isEmpty()) restock();
         CellType next = queue.poll();
         queue.add(stock.poll());
         return TetrominoSprite.get(next);
     }
-    public CellType[] peekWholeQueue(){
+
+    public CellType[] peekWholeQueue() {
         CellType[] cellTypes = new CellType[queue.size()];
         queue.toArray(cellTypes);
         return cellTypes;
+    }
+
+    public CellType peekQueue() {
+        return queue.peekFirst();
     }
 }
